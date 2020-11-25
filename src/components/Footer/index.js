c677f6032f1f9af26cbc828636643bdc44d51023
Jeff T <jeff.thao92@gmail.com>
2020-11-24 21:57:12 -0600

    Added backgroundColor style to make it white. Changed the color of the font awesome icons to grey. Added some spacing between them as well.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,22 +3,24 @@ import { FaGithubAlt, FaLinkedin, FaFileAlt, FaCopyright } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <div className="container-fluid fixed-bottom">
-      <footer className="footer">
+    <div className="container-fluid fixed-bottom" style={{ backgroundColor: "white" }}>
+      <footer className="footer" >
         <p className="text-center text-muted">
-          <FaCopyright style={{fill:'white'}} />
-          Copyright 2020 Jeff Thao
-          <a href="https://github.com/JeffThao" target="_blank">
-          <FaGithubAlt style={{fill:'white'}}/>
+          <FaCopyright style={{fill:'grey'}} />
+          {" "}
+          Copyright 2020 Jeff Thao {" "}
+          <a href="https://github.com/JeffThao" target="_blank" rel="noreferrer" title="Github">
+          <FaGithubAlt style={{fill:'grey'}}/>
           </a>
-          <a href="https://www.linkedin.com/in/jeffthao/" target="_blank">
-          <FaLinkedin style={{fill:'white'}} />
+          {" "}
+          <a href="https://www.linkedin.com/in/jeffthao/" target="_blank" rel="noreferrer" title="Linkedin">
+          <FaLinkedin style={{fill:'grey'}} />
           </a>
+          {" "}
           <a
             href="https://docs.google.com/document/d/12tpNtMYP7WrWKGt5vjewkLY6fyWEhzJ_2RF5LybiFzs/edit?usp=sharing"
-            target="_blank"
-          >
-            <FaFileAlt style={{fill:'white'}} />
+            target="_blank" rel="noreferrer" title="Resume">
+            <FaFileAlt style={{fill:'grey'}} />
           </a>
         </p>
       </footer>
