4cd8c7a7b291dbda8dc51aa3af2bb12fc51b3ec2
Jeff T <jeff.thao92@gmail.com>
2020-11-24 22:21:43 -0600

    changed the color of the navbar to light, changed the icon colors to grey. changed the link with my name into black. changed the about and portfolio links to grey/default.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,19 +7,19 @@ function Nav() {
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link to="/" className={location.pathname === "/" ? "nav-link " : "nav-link"} style={{color:'black'}}>
           Jeff Thao
         </Link>
     <a href="https://github.com/JeffThao" target="_blank" title="Github">
-      <FaGithubAlt style={{fill:'white'}}/>
+      <FaGithubAlt style={{fill:'grey'}}/>
     </a>
     <a href="https://www.linkedin.com/in/jeffthao/" target="_blank" title="Linkedin">
-      <FaLinkedin style={{fill:'white'}} />
+      <FaLinkedin style={{fill:'grey'}} />
     </a>
     <a href="https://docs.google.com/document/d/12tpNtMYP7WrWKGt5vjewkLY6fyWEhzJ_2RF5LybiFzs/edit?usp=sharing"
       target="_parent" rel="noopener" title="Resume">
-        <FaFileAlt style={{fill:'white'}} />
+        <FaFileAlt style={{fill:'grey'}} />
       </a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
       aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,7 +27,7 @@ function Nav() {
     </button>
     <div className="collapse navbar-collapse ml-auto justify-content-end" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <a className="nav-link active" href="/">About <span className="sr-only">(current)</span></a>
+        <a className="nav-link" href="/">About <span className="sr-only"></span></a>
         <a className="nav-link" href="/project">Portfolio</a>
       </div>
     </div>
